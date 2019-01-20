4b94aea6e201695fb6a7fd0911cb08ffdf227748
Rustam Karimov <rkarimov.r@gmail.com>
2019-01-20 22:19:08 +0400

    Implement volume accumulation & tests

--- auction/src/main/java/com/auction/Auction.java
+++ auction/src/main/java/com/auction/Auction.java
@@ -10,23 +10,30 @@ import java.util.TreeMap;
 @Getter
 @Setter
 @NoArgsConstructor
-
 class Auction {
 
     private NavigableMap<Float, Integer> demandBids = new TreeMap<>();
     private NavigableMap<Float, Integer> sellBids = new TreeMap<>();
 
     void addBid(Bid bid) {
-
         BidType bitType = bid.getBidType();
+        float bidPrice = bid.getPrice();
+        int bidVolume = bid.getVolume();
 
         switch (bitType) {
             case DEMAND:
-                demandBids.put(bid.getPrice(), bid.getVolume());
+                updateBidVolume(bidPrice, bidVolume, demandBids);
                 break;
             case SELL:
-                sellBids.put(bid.getPrice(), bid.getVolume());
+                updateBidVolume(bidPrice, bidVolume, sellBids);
                 break;
+        }
+    }
+
+    private void updateBidVolume(float bidPrice, int bidVolume, NavigableMap<Float, Integer> navigableMap) {
+        Integer presentVolume = navigableMap.putIfAbsent(bidPrice, bidVolume);
+        if (presentVolume != null) {
+            navigableMap.put(bidPrice, bidVolume + presentVolume);
         }
     }
 
--- auction/src/test/java/com/auction/AuctionTest.java
+++ auction/src/test/java/com/auction/AuctionTest.java
@@ -1,16 +1,25 @@
 package com.auction;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.NavigableMap;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class AuctionTest {
 
+    private Auction auction;
+
+    @Before
+    public void setup() {
+        auction = new Auction();
+    }
+
     @Test
     public void testEntities() {
-        Auction auction = new Auction();
-        Bid demandBid = new Bid(BidType.DEMAND, 50, 9);
+        Bid demandBid = new Bid(BidType.DEMAND, 50, 9.0f);
         Bid sellBid = new Bid(BidType.SELL, 200, 11.2f);
         auction.addBid(demandBid);
         auction.addBid(sellBid);
@@ -20,6 +29,25 @@ public class AuctionTest {
         assertEquals(sellBid.getBidType(), BidType.SELL);
         assertEquals(sellBid.getPrice(), 11.2f, 0.0f);
         assertEquals(demandBid.getVolume(), 50);
+    }
+
+    @Test
+    public void testVolumeAccumulation() {
+        // given
+        for (int i = 0; i < 3; i++) {
+            auction.addBid(new Bid(BidType.DEMAND, 10, 1.0f));
+            auction.addBid(new Bid(BidType.SELL, 1, 1.0f));
+        }
+        NavigableMap<Float, Integer> demandBids = auction.getDemandBids();
+        NavigableMap<Float, Integer> sellBids= auction.getSellBids();
+
+        // when
+        Integer demandBidsVolume = demandBids.get(1.0f);
+        Integer sellBidsVolume = sellBids.get(1.0f);
+
+        // then
+        assertEquals(demandBidsVolume, Integer.valueOf(3 * 10));
+        assertEquals(sellBidsVolume, Integer.valueOf(3));
     }
 
 
