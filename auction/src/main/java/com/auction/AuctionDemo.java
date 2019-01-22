1fdff85a48c896ffdcb3bcfc00f1e00076b0b028
Rustam Karimov <rkarimov.r@gmail.com>
2019-01-22 12:04:42 +0400

    Add input file reader

--- auction/src/main/java/com/auction/AuctionDemo.java
+++ auction/src/main/java/com/auction/AuctionDemo.java
@@ -5,18 +5,54 @@ import com.auction.entity.Bid;
 import com.auction.entity.Type;
 import com.auction.exceptions.AuctionImpossibleException;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class AuctionDemo {
 
     public static void main(String[] args) {
-        Auction auction = new Auction();
-        Bid demandBid = new Bid(Type.DEMAND, 50, 9f);
-        Bid sellBid = new Bid(Type.SELL, 200, 11.2f);
-        auction.addBid(demandBid);
-        auction.addBid(sellBid);
         try {
-            System.out.println(auction.getMaximumVolume());
+            BufferedReader in = new BufferedReader(new FileReader(args[0]));
+            Auction auction = readInputData(in);
+            Integer maximumVolume = auction.getMaximumVolume();
+            Float minimumPrice = auction.getMinimumPrice();
+            System.out.println(maximumVolume + " " + minimumPrice);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Your should provide input file as argument!");
         } catch (AuctionImpossibleException e) {
             System.out.println("0 n/a");
+        } catch (FileNotFoundException e) {
+            System.out.println(String.format("File '%s' not found!", args[0]));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    private static Auction readInputData(BufferedReader in) throws IOException {
+        Auction output = new Auction();
+        String line;
+        StringTokenizer stk;
+        String type;
+        int volume;
+        float price;
+        while ((line = in.readLine()) != null) {
+            stk = new StringTokenizer(line);
+            type = stk.nextToken();
+            volume = Integer.parseInt(stk.nextToken());
+            price = Float.parseFloat(stk.nextToken());
+
+            switch (type) {
+                case "B":
+                    output.addBid(new Bid(Type.DEMAND, volume, price));
+                    break;
+                case "S":
+                    output.addBid(new Bid(Type.SELL, volume, price));
+                    break;
+            }
+        }
+        return output;
     }
 }
